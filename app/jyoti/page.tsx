cc865ae2a32761d05124bc3d4f73bab229dfae4b
07jhashubham <07jha30@gmail.com>
2024-01-16 18:00:08 +0530

    Large

---
+++ app/jyoti/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jyoti",
+  description: "Jyoti Meta data",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <div>gdfg</div>
+      dib dffgdf
+      <div>fgsdf</div>
+    </div>
+  );
+}
